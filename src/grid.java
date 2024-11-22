7c5a937e5e09a88395bb15f85e89d04e0903ebc6
EdisonHuang810 <edison.huangb2026@stu.tes.tp.edu.tw>
2024-11-22 10:42:35 +0800

    refined Grid

--- src/grid.java
+++ src/grid.java
@@ -3,16 +3,23 @@ public static void main(String[] args){
     int rows = 7;
     int cols = 15;
     char[][] grid = new char[rows][cols];
-    grid[5][5] = 's'; 
+    
 
 
     for(int i=0;i< rows;i++){
         for(int k=0;k<cols;k++){
-            grid[i][k] = '-';
-            System.out.print(grid[i][k] + "");
+            grid[i][k] = '*';
+        }
+    }
+    grid[3][3] = 'O';
+    
+    for(int i=0;i<rows;i++){
+        for(int k=0;k<cols;k++){
+            System.out.print(grid[i][k]);
         }
         System.out.println("");
     }
 }
+
     }
 
